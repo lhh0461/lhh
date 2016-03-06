18c100e06c2c964e797f7fc5841010df2134ccce
lhh <lhh0461@175game.com>
2016-03-06 06:39:57 -0800

    删掉移除文件

--- src/config.h
+++
@@ -1,8 +0,0 @@
-
-void config_init();
-static void __init_config(void);
-void config_set_int(const char *key, int value);
-int config_get_int(const char *key);
-void config_set_string(const char *key, const char *value);
-const char *config_get_string(const char *key);
-
